495d9b37877034db041a4b7fb042d15602a5b884
Hrithik Naha <naha.hrithkk@gmail.com>
2021-05-06 00:51:26 +0530

    add home and styles

---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Searchbar from "./Searchbar";
+
+function Home() {
+	return (
+		<div>
+			<Searchbar />
+		</div>
+	);
+}
+
+export default Home;
---
+++ src/components/List.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+function List() {
+	return (
+		<div className="list">
+			<p>artistName</p>
+			<p>artistViewUrl</p>
+			<p>artworkUrl30</p>
+			<p>kind</p>
+			<p>releaseDate</p>
+			<p>trackName</p>
+			<p>trackTime</p>
+			<p>trackPrice</p>
+		</div>
+	);
+}
+
+export default List;
